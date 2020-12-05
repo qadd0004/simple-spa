e46dc26737596da1f1a21b6ed7334faafa0632f5
qadd0004 <qadd0004@algonquinlive.com>
2020-12-05 17:17:09 -0500

    Add media page and test and modify the app to fulfill all required features

--- js/main.js
+++ js/main.js
@@ -1,38 +1,40 @@
+//API api_key=803734898f6659797a0f7e7dc6a24147
+
 //app is for general control over the application
 //and connections between the other components
-// My API api_key=803734898f6659797a0f7e7dc6a24147
-//Example request https://api.themoviedb.org/3/movie/550?api_key=803734898f6659797a0f7e7dc6a24147
-// image request  https://api.themoviedb.org/3/configuration?api_key={apikey}
+
 const APP = {
   init() {
     //this function runs when the page loads
     let searchBox = document.querySelector("#search");
     let searchBtn = document.querySelector("#btnSearch");
 
+    searchBox.addEventListener("focus", (ev) => {
+      searchBtn.classList.remove("pulse");
+    });
+
+    searchBox.addEventListener("blur", (ev) => {
+      searchBtn.classList.add("pulse");
+    });
+
     searchBtn.addEventListener("click", (ev) => {
       ev.preventDefault();
       name = searchBox.value;
+      searchBtn.style.outline = "none";
+      PAGES.switchPage(PAGES["currentPage"], PAGES["actors"]);
       if (name) {
+        SEARCH.getConfiguration();
         ACTORS.getActor(name);
-        PAGES.switchPage(PAGES.instr, PAGES.actors);
         searchBox.value = "";
       }
     });
   },
 };
 
-//search is for anything to do with the fetch api
-const SEARCH = {
-  results: [],
-};
-
-//console.log(actors);
 //actors is for changes connected to content in the actors section
 const ACTORS = {
   getActor(actor) {
     let url = `https://api.themoviedb.org/3/search/person?api_key=803734898f6659797a0f7e7dc6a24147&language=en-US&query=${actor}&page=1&include_adult=false`;
-
-    //open(url);
 
     fetch(url)
       .then(function (response) {
@@ -43,8 +45,8 @@ const ACTORS = {
         }
       })
       .then(function (data) {
-        ACTORS.card(data);
-        SEARCH.results.push(data);
+        SEARCH.results[actor] = data.results;
+        ACTORS.card(SEARCH.results[actor]);
       })
       .catch((err) => {
         //handle the error
@@ -52,52 +54,188 @@ const ACTORS = {
       });
   },
 
-  card(result) {
-    if (result["total_results"] === 0) {
+  card(result, config) {
+    config = SEARCH.config;
+    if (result.length === 0) {
       alert(
-        "There is no actor with that name, please check your spelling and try again"
+        `There is no actor/actress with that name, please check your spelling and try again`
       );
-      PAGES.switchPage(PAGES.actors, PAGES.instr);
+      PAGES.switchPage(PAGES["currentPage"], PAGES["instr"]);
       return;
-    }
+    } else {
+      let list = document.querySelector("#actorsList");
+      list.innerHTML = "";
 
-    let list = document.querySelector("#actorsList");
+      result.forEach((actorObject) => {
+        let card = document.createElement("div");
+        card.classList.add("card");
+        card.setAttribute("data-id", actorObject.id);
+
+        let picture = document.createElement("div");
+        picture.classList.add("picture");
+
+        let image = document.createElement("img");
+        if (actorObject.profile_path) {
+          image.src = `${config["images"]["secure_base_url"]}${config["images"]["profile_sizes"][2]}${actorObject["profile_path"]}`;
+        }
+        image.alt = actorObject.name;
+
+        picture.append(image);
+        card.append(picture);
+
+        let h3 = document.createElement("h3");
+        h3.classList.add("name");
+        h3.textContent = actorObject.name;
+        card.append(h3);
+
+        let popularity = document.createElement("p");
+        popularity.classList.add("pop");
+        let pop = Math.ceil(actorObject.popularity / 3);
+        pop = pop <= 5 ? pop : 5;
+        popularity.innerHTML = "<span>&star;</span>".repeat(pop);
+        card.append(popularity);
+
+        // check for image existence before appending card
+        if (actorObject.id && actorObject.profile_path) {
+          list.append(card);
+          PAGES.switchPage(PAGES["currentPage"], PAGES["actors"]);
+        } else if (!actorObject.profile_path && list.children.length == 0) {
+          h3.textContent = `As ${
+            actorObject.name
+          } has no profile picture no search result's cards will be shown for ${
+            actorObject.gender === 2 ? "him" : "her"
+          }`;
+          list.append(h3);
+          PAGES.switchPage(PAGES["currentPage"], PAGES["actors"]);
+        }
+
+        card.addEventListener("click", (ev) => MEDIA.getMedia(ev, actorObject));
+      });
+    }
+  },
+};
+
+//search is for anything to do with the fetch api
+const SEARCH = {
+  config: null,
+  getConfiguration() {
+    url =
+      "https://api.themoviedb.org/3/configuration?api_key=803734898f6659797a0f7e7dc6a24147";
+
+    fetch(url)
+      .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Bad response", response.status);
+        }
+      })
+      .then(function (data) {
+        SEARCH.config = data;
+      })
+      .catch((err) => {
+        //handle the error
+        alert(err);
+      });
+  },
+  // results contain named objects for easier access.
+  results: {},
+};
+
+//media is for changes connected to content in the media section
+const MEDIA = {
+  media: null,
+  getMedia(ev, actor) {
+    let h3 = document.querySelector("#media h3");
+    let clickedActorId = ev.currentTarget.getAttribute("data-id");
+    // checking for Actor ID before retrieving his data
+    if (parseInt(clickedActorId) === actor.id) {
+      h3.textContent = `${actor["name"]} is best known for:`;
+
+      MEDIA.media = actor["known_for"];
+      if (MEDIA.media.length > 0) {
+        MEDIA.addMedia(media);
+        PAGES.switchPage(PAGES["actors"], PAGES["media"]);
+        MEDIA.setDimensions();
+
+        let actorsBtn = document.querySelector("h1 span");
+        actorsBtn.classList.add("act");
+        actorsBtn.addEventListener("click", showActors);
+
+        function showActors() {
+          PAGES.switchPage(PAGES["currentPage"], PAGES["actors"]);
+          removeActorsListener();
+        }
+        function removeActorsListener() {
+          actorsBtn.classList.remove("act");
+          actorsBtn.removeEventListener("click", showActors);
+        }
+      } else {
+        alert("Actor's media shows/movies not found");
+      }
+    } else {
+      alert("Actor not found");
+    }
+  },
+  addMedia(media) {
+    media = MEDIA.media;
+    let list = document.querySelector("#titles");
     list.innerHTML = "";
 
-    result.results.forEach((actor) => {
+    media.forEach((show) => {
+      // show is either a movie with a title property or a TV show with a name property
+      let data_id = show["id"];
+      let name = show["name"] || show["title"];
+      let poster = show["poster_path"];
+      let yearShown = show["first_air_date"] || show["release_date"];
+      yearShown = yearShown.slice(0, 4);
+
       let card = document.createElement("div");
       card.classList.add("card");
-      card.setAttribute("data-id", actor.id);
+      card.setAttribute("data-id", data_id);
 
       let picture = document.createElement("div");
       picture.classList.add("picture");
 
       let image = document.createElement("img");
-      image.src = "https://image.tmdb.org/t/p/w185/" + actor.profile_path;
-      image.alt = actor.name;
+      image.src = `${SEARCH["config"]["images"]["secure_base_url"]}${SEARCH["config"]["images"]["poster_sizes"][2]}${poster}`;
+      image.alt = name;
 
       picture.append(image);
       card.append(picture);
 
-      let h3 = document.createElement("h3");
-      h3.classList.add("name");
-      h3.textContent = actor.name;
-      card.append(h3);
+      let cardH3 = document.createElement("h3");
+      cardH3.classList.add("name");
+      cardH3.textContent = name;
+      card.append(cardH3);
 
-      let popularity = document.createElement("p");
-      popularity.classList.add("pop");
-      let pop = Math.ceil(actor.popularity / 3);
-      pop = pop <= 5 ? pop : 5;
-      popularity.innerHTML = "<span>&star;</span>".repeat(pop);
-      card.append(popularity);
-
+      let year = document.createElement("p");
+      year.classList.add("pop");
+      year.textContent = yearShown;
+      card.append(year);
       list.append(card);
     });
   },
+  setDimensions() {
+    let mediaPage = document.querySelector("#media");
+    let header = document.querySelector("#header");
+    let headerHeight = header.getBoundingClientRect().height;
+    mediaPage.style.top = `${headerHeight + 30}px`;
+  },
 };
 
-//media is for changes connected to content in the media section
-const MEDIA = {};
+// PAGES is for switching between pages
+const PAGES = {
+  currentPage: document.querySelector(".active"),
+  instr: document.querySelector("#instructions"),
+  actors: document.querySelector("#actors"),
+  media: document.querySelector("#media"),
+  switchPage(prev, next) {
+    prev.classList.remove("active");
+    next.classList.add("active");
+    PAGES.currentPage = next;
+  },
+};
 
 //storage is for working with localstorage
 const STORAGE = {
@@ -109,16 +247,5 @@ const NAV = {
   //this will be used in Assign 4
 };
 
-const PAGES = {
-  instr: document.querySelector("#instructions"),
-  actors: document.querySelector("#actors"),
-  media: document.querySelector("#actors"),
-  switchPage(prev, next) {
-    prev.classList.remove("active");
-    next.classList.add("active");
-  },
-};
-
 //Start everything running
-
-document.addEventListener("DOMContentLoaded", APP.init);
+APP.init();
